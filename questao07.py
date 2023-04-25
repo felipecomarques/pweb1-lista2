59205842c19801aa9ce6bfb97db72d16277f6ebc
Felipe Marques <felipe.comarques1208@gmail.com>
2023-04-24 23:12:47 -0300

    Correção 07

--- questao07.py
+++ questao07.py
@@ -1,8 +1,8 @@
 numero = int(input("Insira um numero: "))
-f0, f1, fi = 0,1,0
+f0, f1, fi = 1,0,0
 
 print("A sequencia de Fibonacci ate esse numero eh:")
-for i in range(numero+1):
+for i in range(numero):
     fi = f0 + f1   
     f0 = f1
     f1 = fi
